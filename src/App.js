6df2ef694b31e67e9e6a3373409e61fe5804ae23
Kham En <khamen28@gmail.com>
2022-02-23 12:51:02 -0500

    Implement a static verion

--- src/App.js
+++ src/App.js
@@ -1,5 +1,64 @@
+function ReportCard() {
+  return (
+    <div>
+      <p>Report of everything</p>
+      <button type="submit">Finish</button>
+    </div>
+  );
+}
+
+function Result() {
+  return (
+    <div>
+      <p>user input === answer</p>
+      <button type="submit">Next</button>
+    </div>
+  );
+}
+
+function Answer() {
+  return (
+    <div>
+      <input type="text" placeholder="User is typing..." />
+      <button type="submit">Submit</button>
+    </div>
+  );
+}
+
+function Question({ letter }) {
+  return <p>What is the telephony of {letter}</p>;
+}
+
+const mockLetter = "j";
+
+function Quizzer() {
+  return (
+    <section>
+      <Question letter={mockLetter} />
+      <Answer />
+      {/*<Result />*/}
+    </section>
+  );
+}
+
+function Scoreboard({ score }) {
+  return <section>The score is: {score}</section>;
+}
+
+const mockScore = "3 out 26";
+
+function NatoGame() {
+  return (
+    <form>
+      <Scoreboard score={mockScore} />
+      {/*<Quizzer />*/}
+      <ReportCard />
+    </form>
+  );
+}
+
 function App() {
-  return <div></div>;
+  return <NatoGame />;
 }
 
 export default App;
